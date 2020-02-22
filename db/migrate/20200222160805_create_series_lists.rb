db74691d33e8343643faecbfea558a5218fa4d7b
Tauhir Edwards <tauhir.edwards@gmail.com>
2020-02-22 22:55:08 +0200

    Updated migrations to fix errors.

--- db/migrate/20200222160805_create_series_lists.rb
+++ db/migrate/20200222160805_create_series_lists.rb
@@ -1,10 +1,10 @@
 class CreateSeriesLists < ActiveRecord::Migration[6.0]
   def change
     create_table :series_lists do |t|
-      t.string :api_id,
-      t.string :name,language,origin_country
-      t.string :language,
-      t.string[] :external_series
+      t.string :api_id
+      t.string :name
+      t.string :language
+      t.t.string :external_series, array: true, default: [] 
       t.timestamps
     end
   end
