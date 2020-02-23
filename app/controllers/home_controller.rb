40d0cbb921e6542291c5c6d5257d12a4fa686f18
Tauhir Edwards <tauhir.edwards@gmail.com>
2020-02-23 21:18:58 +0200

    Setup bootstrap and start on homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def new
+  	# homepage, allow to search from the new view or select genre or select from a few top randos e.g got, friends 
   end
 
   def index
