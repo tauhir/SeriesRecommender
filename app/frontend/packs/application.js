1d1e102c0107f3285d0f57f26a997a64eaab1625
Tauhir Edwards <tauhir.edwards@gmail.com>
2020-03-03 00:01:36 +0200

    webpacker working

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,11 +7,26 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require.context('../packs/images', true);
+window.jQuery = window.$ = require('jquery')
+import 'bootstrap/dist/js/bootstrap';
+import FooImage from 'packs/images/image.png'
 
+//console.log("Hello from hello.js");
 
+function noImage(element) {
+  //console.log("Hello from func");
+  element.src=FooImage;
+}
+// @TODO, why is window constantly needed? See this as starting point
+//https://stackoverflow.com/questions/60048206/why-are-my-js-erb-views-not-working-when-using-webpacker-in-rails-6-with-bootstr
+window.noImage = noImage;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+
