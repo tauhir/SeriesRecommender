e82bc9b60cec56cd3ed71346ed8967397bb1536a
Tauhir Edwards <tauhir.edwards@gmail.com>
2020-03-05 22:13:02 +0200

    updated no image file, built js hover functionality

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -19,9 +19,20 @@ function noImage(element) {
   //console.log("Hello from func");
   element.src=FooImage;
 }
+
+function showInfo(element) {
+	element.querySelector(".inner").style.display = "block";
+
+}
+
+function hideInfo(element) {
+	element.querySelector(".inner").style.display = "none";
+}
 // @TODO, why is window constantly needed? See this as starting point
 //https://stackoverflow.com/questions/60048206/why-are-my-js-erb-views-not-working-when-using-webpacker-in-rails-6-with-bootstr
 window.noImage = noImage;
+window.hideInfo = hideInfo;
+window.showInfo = showInfo;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -29,5 +40,13 @@ window.noImage = noImage;
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
+$( document ).ready(function() {
+    $('.card-img-top').hover(
+        function() {
+    $('.inner').fadeIn();
+  }, function() {
+   $('.inner').fadeOut();
+  }
+    );
+});
 
